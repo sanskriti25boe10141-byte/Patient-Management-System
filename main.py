2361a3df851e2b81f5728e3a1d84c5a7f83a7850
sanskriti25boe10141-byte <sanskriti.25boe10141@vitbhopal.ac.in>
2026-06-13 14:25:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,45 @@ from database import load_data, save_data
 
 patients = load_data()
 
-print("Patient Management System Started")
+def add_patient():
+
+    pid = input("Enter Patient ID: ")
+    name = input("Enter Name: ")
+    age = input("Enter Age: ")
+    gender = input("Enter Gender: ")
+    blood_group = input("Enter Blood Group: ")
+    phone = input("Enter Phone Number: ")
+    disease = input("Enter Disease: ")
+
+    patient = Patient(
+        pid,
+        name,
+        age,
+        gender,
+        blood_group,
+        phone,
+        disease
+    )
+
+    patients.append(patient.to_dict())
+    save_data(patients)
+
+    print("\nPatient Added Successfully!")
+
+while True:
+
+    print("\n===== Patient Management System =====")
+    print("1. Add Patient")
+    print("2. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        add_patient()
+
+    elif choice == "2":
+        print("Exiting...")
+        break
+
+    else:
+        print("Invalid Choice")
