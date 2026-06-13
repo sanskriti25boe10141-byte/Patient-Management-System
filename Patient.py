ef7c06a7064245fa46ecf34ac2fe6315fba9035c
sanskriti25boe10141-byte <sanskriti.25boe10141@vitbhopal.ac.in>
2026-06-13 14:07:51 +0530

    Add Patient class with initialization and to_dict method

--- Patient.py
+++ Patient.py
@@ -1,1 +1,20 @@
+class Patient:
+    def __init__(self, pid, name, age, gender, blood_group, phone, disease):
+        self.pid = pid
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.blood_group = blood_group
+        self.phone = phone
+        self.disease = disease
 
+    def to_dict(self):
+        return {
+            "pid": self.pid,
+            "name": self.name,
+            "age": self.age,
+            "gender": self.gender,
+            "blood_group": self.blood_group,
+            "phone": self.phone,
+            "disease": self.disease
+        }
